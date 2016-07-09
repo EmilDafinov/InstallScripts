b3aae3ac05d0cd5ac8f211364d5d4f4e58a8f4c8
emiliorodo <emiliorodo@gmail.com>
2016-07-09 11:04:51 -0400

    Update installs.sh: add brew bundle

--- installs.sh
+++ installs.sh
@@ -4,3 +4,7 @@ curl -L http://install.ohmyz.sh | sh
 
 #Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#Homebrew bundle
+brew tap Homebrew/bundle
+brew bundle
