68b4f4ca4f75d569c02356783176c0ecad6368d4
emiliorodo <emiliorodo@gmail.com>
2016-07-09 13:00:16 -0400

    Update installs.sh to add mas

--- installs.sh
+++ installs.sh
@@ -5,6 +5,8 @@ curl -L http://install.ohmyz.sh | sh
 #Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew install mas
+
 #Homebrew bundle
 brew tap Homebrew/bundle
 brew bundle
